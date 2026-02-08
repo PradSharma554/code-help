6c8e2c3f329685b5e973103e90199110730a1c77
Pradyuman554 <pradyuman.sharma@zop.dev>
2026-02-08 20:37:35 +0530

    remove

--- test-leetcode-stats.js
+++
@@ -1,9 +0,0 @@
-const { getUserStats } = require("./lib/leetcode");
-
-async function test() {
-  console.log("Fetching stats for 'tmwilliamlin168'...");
-  const stats = await getUserStats("tmwilliamlin168");
-  console.log("Stats:", JSON.stringify(stats, null, 2));
-}
-
-test();
